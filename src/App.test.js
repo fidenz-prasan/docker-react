864131f4c8eb06b3aab0de63734f45059158dcaa
fidenz-prasan <prasan.n@sms.fidenz.com>
2022-09-06 10:01:13 +0530

    changed tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hii there!/i);
+  const linkElement = screen.getByText(/Im in AWS now!/i);
   expect(linkElement).toBeInTheDocument();
 });
