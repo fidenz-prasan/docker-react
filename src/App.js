4c21454ca83731028f0d15d8ea23cb0104f5af12
fidenz-prasan <99183064+fidenz-prasan@users.noreply.github.com>
2022-09-06 10:07:15 +0530

    changed text (#1)
    * changed text
    
    * changed tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hii there!
+          Im in AWS now!
         </a>
       </header>
     </div>
